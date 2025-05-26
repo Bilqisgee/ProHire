6c73854e018177f9c8025c902c9a89be09469207
Bilqisgee <bilqisgee@gmail.com>
2025-05-26 18:09:14 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use("/api/user/service", UserProfileRoutes)
 
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
     app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+        res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
     })
 
 }
